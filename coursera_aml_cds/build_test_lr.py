7abfb906921a43e2244c78c7cfc1caa73c84fb6b
stehem <stehem@fastmail.fm>
2019-03-31 14:30:42 +0200

    1.12

--- coursera_aml_cds/build_test_lr.py
+++ coursera_aml_cds/build_test_lr.py
@@ -1,15 +1,20 @@
+import numpy as np
 
-def build_test_f(interval, test, training_test, features, window_size):
+def build_test_lr_f(interval, test, training_test, features):
     print(interval)
     test_lstm_data = []
     for index, row in test[interval[0]:interval[1]].iterrows():
         #if index % 10000 == 0:
         #print(index)
-        line = training_test[(training_test['shop_id'] == row['shop_id']) & (training_test['item_id'] == row['item_id'])]\
-                .sort_values(by=['date_block_num'])[features].values
+        line = training_test[(training_test['shop_id'] == row['shop_id']) &\
+                             (training_test['item_id'] == row['item_id'])].sort_values(by=['date_block_num'])[features]
+        avgs = []
+        for feature in features:
+            feature_avg = np.average(line[feature].values, weights=[1,2,4,8,16,32,64], axis=0)
+            avgs.append(feature_avg)
        # if len(line) != 10:
             #print(len(line), row['shop_id'], row['item_id'])
         #print(line)
-        test_lstm_data.append(line[1:window_size+1])
+        test_lstm_data.append(avgs)
         
     return {interval: test_lstm_data}
