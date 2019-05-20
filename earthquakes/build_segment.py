1aaf5ff8bb84f5136277495f59b7926181456ceb
stephane <stehem@fastmail.fm>
2019-05-20 17:17:12 +0200

    WIP

--- earthquakes/build_segment.py
+++ earthquakes/build_segment.py
@@ -1,6 +1,21 @@
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler,MinMaxScaler 
+import itertools
+
+
+def _roll(a, shift):
+    if not isinstance(a, np.ndarray):
+        a = np.asarray(a)
+    idx = shift % len(a)
+    return np.concatenate([a[-idx:], a[:-idx]])
+
+def _get_length_sequences_where(x):
+    if len(x) == 0:
+        return [0]
+    else:
+        res = [len(list(group)) for value, group in itertools.groupby(x) if value == 1]
+        return res if len(res) > 0 else [0]
 
 #https://tsfresh.readthedocs.io/en/latest/_modules/tsfresh/feature_extraction/feature_calculators.html#autocorrelation
 def autocorrelation(x, lag):
@@ -71,6 +86,177 @@ def skewness(x):
         x = pd.Series(x)
     return pd.Series.skew(x)
 
+def longest_strike_below_mean(x):
+    if not isinstance(x, (np.ndarray, pd.Series)):
+        x = np.asarray(x)
+    return np.max(_get_length_sequences_where(x <= np.mean(x))) if x.size > 0 else 0
+
+
+def longest_strike_above_mean(x):
+    if not isinstance(x, (np.ndarray, pd.Series)):
+        x = np.asarray(x)
+    return np.max(_get_length_sequences_where(x >= np.mean(x))) if x.size > 0 else 0
+
+
+def count_above_mean(x):
+    m = np.mean(x)
+    return np.where(x > m)[0].size
+
+
+def count_below_mean(x):
+    m = np.mean(x)
+    return np.where(x < m)[0].size
+
+
+def last_location_of_maximum(x):
+    x = np.asarray(x)
+    return 1.0 - np.argmax(x[::-1]) / len(x) if len(x) > 0 else np.NaN
+
+
+def first_location_of_maximum(x):
+    if not isinstance(x, (np.ndarray, pd.Series)):
+        x = np.asarray(x)
+    return np.argmax(x) / len(x) if len(x) > 0 else np.NaN
+
+
+def last_location_of_minimum(x):
+    x = np.asarray(x)
+    return 1.0 - np.argmin(x[::-1]) / len(x) if len(x) > 0 else np.NaN
+
+
+def first_location_of_minimum(x):
+    if not isinstance(x, (np.ndarray, pd.Series)):
+        x = np.asarray(x)
+    return np.argmin(x) / len(x) if len(x) > 0 else np.NaN
+
+
+def percentage_of_reoccurring_datapoints_to_all_datapoints(x):
+    if len(x) == 0:
+        return np.nan
+
+    unique, counts = np.unique(x, return_counts=True)
+
+    if counts.shape[0] == 0:
+        return 0
+
+    return np.sum(counts > 1) / float(counts.shape[0])
+
+
+
+def percentage_of_reoccurring_values_to_all_values(x):
+    if not isinstance(x, pd.Series):
+        x = pd.Series(x)
+
+    if x.size == 0:
+        return np.nan
+
+    value_counts = x.value_counts()
+    reoccuring_values = value_counts[value_counts > 1].sum()
+
+    if np.isnan(reoccuring_values):
+        return 0
+
+    return reoccuring_values / x.size
+
+
+
+def sum_of_reoccurring_values(x):
+    unique, counts = np.unique(x, return_counts=True)
+    counts[counts < 2] = 0
+    counts[counts > 1] = 1
+    return np.sum(counts * unique)
+
+
+
+def sum_of_reoccurring_data_points(x):
+    unique, counts = np.unique(x, return_counts=True)
+    counts[counts < 2] = 0
+    return np.sum(counts * unique)
+
+
+def ratio_value_number_to_time_series_length(x):
+    if not isinstance(x, (np.ndarray, pd.Series)):
+        x = np.asarray(x)
+    if x.size == 0:
+        return np.nan
+    return np.unique(x).size / x.size
+
+def number_peaks(x, n):
+    """
+    Calculates the number of peaks of at least support n in the time series x. A peak of support n is defined as a
+    subsequence of x where a value occurs, which is bigger than its n neighbours to the left and to the right.
+
+    Hence in the sequence
+
+    >>> x = [3, 0, 0, 4, 0, 0, 13]
+
+    4 is a peak of support 1 and 2 because in the subsequences
+
+    >>> [0, 4, 0]
+    >>> [0, 0, 4, 0, 0]
+
+    4 is still the highest value. Here, 4 is not a peak of support 3 because 13 is the 3th neighbour to the right of 4
+    and its bigger than 4.
+
+    :param x: the time series to calculate the feature of
+    :type x: numpy.ndarray
+    :param n: the support of the peak
+    :type n: int
+    :return: the value of this feature
+    :return type: float
+    """
+    x_reduced = x[n:-n]
+
+    res = None
+    for i in range(1, n + 1):
+        result_first = (x_reduced > _roll(x, i)[n:-n])
+
+        if res is None:
+            res = result_first
+        else:
+            res &= result_first
+
+        res &= (x_reduced > _roll(x, -i)[n:-n])
+    return np.sum(res)
+
+def sample_entropy(x):
+    x = np.array(x)
+
+    sample_length = 1 # number of sequential points of the time series
+    tolerance = 0.2 * np.std(x) # 0.2 is a common value for r - why?
+
+    n = len(x)
+    prev = np.zeros(n)
+    curr = np.zeros(n)
+    A = np.zeros((1, 1))  # number of matches for m = [1,...,template_length - 1]
+    B = np.zeros((1, 1))  # number of matches for m = [1,...,template_length]
+
+    for i in range(n - 1):
+        nj = n - i - 1
+        ts1 = x[i]
+        for jj in range(nj):
+            j = jj + i + 1
+            if abs(x[j] - ts1) < tolerance:  # distance between two vectors
+                curr[jj] = prev[jj] + 1
+                temp_ts_length = min(sample_length, curr[jj])
+                for m in range(int(temp_ts_length)):
+                    A[m] += 1
+                    if j < n - 1:
+                        B[m] += 1
+            else:
+                curr[jj] = 0
+        for j in range(nj):
+            prev[j] = curr[j]
+
+    N = n * (n - 1) / 2
+    B = np.vstack(([N], B[0]))
+
+    # sample entropy = -1 * (log (A/B))
+    similarity_ratio = A / B
+    se = -1 * np.log(similarity_ratio)
+    se = np.reshape(se, -1)
+    return se[0]
+
 
 def build_training_sample(df: pd.DataFrame, number_of_groups: int, scale: bool) -> pd.DataFrame:
     acoustic_data = np.array_split(df['acoustic_data'].astype("float").values, number_of_groups)
@@ -103,7 +289,20 @@ def build_training_sample(df: pd.DataFrame, number_of_groups: int, scale: bool) 
         percentage_of_reoccurring_datapoints_to_all_datapoints(sub),
         ratio_beyond_r_sigma(sub,2),
         ratio_value_number_to_time_series_length(sub),
-        skewness(sub)
+        skewness(sub),
+        longest_strike_below_mean(sub),
+        longest_strike_above_mean(sub),
+        last_location_of_maximum(sub),
+        first_location_of_maximum(sub),
+        last_location_of_minimum(sub),
+        first_location_of_minimum(sub),
+        percentage_of_reoccurring_datapoints_to_all_datapoints(sub),
+        percentage_of_reoccurring_values_to_all_values(sub),
+        sum_of_reoccurring_values(sub),
+        sum_of_reoccurring_data_points(sub),
+        ratio_value_number_to_time_series_length(sub),
+        number_peaks(sub,100),
+        #sample_entropy(sub)
     ) for sub in acoustic_data])
 
     df2 = pd.DataFrame(data)
@@ -123,7 +322,20 @@ def build_training_sample(df: pd.DataFrame, number_of_groups: int, scale: bool) 
                    "reocurring_pct",
                    "r_sigma",
                    "ratio_to_length",
-                   "skewness"
+                   "skewness",
+                   "strike_below",
+                   "strike_above",
+                   "last_loc_max",
+                   "first_loc_max",
+                   "last_loc_min",
+                   "first_loc_min",
+                   "perc_reocurr_dp",
+                   "perc_reocurr_all",
+                   "sum_reoccurr_val",
+                   "sum_reoccurr_dp",
+                   "ratio_value_number",
+                   "peaks",
+                   #"entropy"
                   ]
 
 
